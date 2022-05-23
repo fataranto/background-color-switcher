d4618d9ed5514a64dbf898e5f2ac8adb8413e749
fataranto <fataranto@gmail.com>
2022-05-23 19:36:09 +0200

    final - revisar

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 // Recuperamos TODOS los elementos HTML que cumplen con la condición class="button"
 const buttons = document.querySelectorAll('.button');
 const body = document.querySelector('body');
+
+
+buttons.forEach(element => {
+    this.addEventListener("click", function(e){
+        //e.target.style.backgroundColor = 'red'
+        //myElement = document.querySelector("#"+e.target.id)   ;
+        //console.log(myElement.style);
+        //console.dir(e.target.outerHTML);
+        body.style.backgroundColor = e.target.id;
+        //body.style.backgroundColor = e.target.style.backgroundColor;
+    })
+});
+
